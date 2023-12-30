3d67409050adc6ad8a5b6c24bf65884b9539ec96
Flye001 <philip.org1@gmail.com>
2023-12-30 14:19:49 +0000

    Added basic triangle sruct

---
+++ Triangle.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK.Mathematics;
+
+namespace OpenTkEngine
+{
+    internal struct Triangle
+    {
+        public Vector3 Point1;
+        public Vector3 Point2;
+        public Vector3 Point3;
+
+        public Triangle()
+        {
+            Point1 = new Vector3();
+            Point2 = new Vector3();
+            Point3 = new Vector3();
+        }
+
+        public Triangle(Vector3[] points)
+        {
+            Point1 = points[0];
+            Point2 = points[1];
+            Point3 = points[2];
+        }
+    }
+}
